9f63c9be7c96710687f47368bdd2c7013531d67c
Zianii <41494410+Zianii@users.noreply.github.com>
2019-02-06 18:29:36 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -88,6 +88,52 @@ client.user.setStatus("offline")
 });
 
 
+client.on('message', message => {//new msg event
+    if(!message.channel.guild) return;
+      if(message.content.startsWith(prefix + 'cRainbow')) {//to create the rainbow role
+          let role = message.guild.roles.find('name', 'Rainbow')
+        if(role) return message.channel.send(`This Step Already Completed !`)//if the role already created return with this msg
+      //start of create role 
+      if(!role){
+        rainbow =  message.guild.createRole({
+       name: "Rainbow",//the role will create name
+       color: "#000000",//the default color
+       permissions:[]//the permissions
+     //end of create role
+    })
+    
+    }
+    message.channel.send('Done The Rainbow Role Setup Has Been Completed')//if the step completed
+}})
+
+client.on('ready', () => {//new ready event
+    setInterval(function(){
+        client.guilds.forEach(g => {
+                    var role = g.roles.find('name', 'Rainbow');//rainbow role name
+                    if (role) {
+                        role.edit({color : "RANDOM"});
+                    };
+        });
+    }, 5000);//the rainbow time
+  })
+  
+    
+  client.on("message", message => {
+    if (!message.guild) return;
+    const Rainbow = message.guild.roles.find(role => role.name === "Rainbow");
+    if(message.content.startsWith(prefix + 'rainbow')) {
+    if(message.guild.member(message.author).roles.has(Rainbow)) return;
+    message.member.removeRole(Rainbow);
+    }
+    else
+    if(message.guild.member(message.author).roles.has(Rainbow));
+    return message.member.addRole(Rainbow);
+    }
+    
+
+);
+
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
