06d35575173f9d568b99a44027a9967794c70ccb
Zianii <41494410+Zianii@users.noreply.github.com>
2019-02-06 18:32:12 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -114,7 +114,7 @@ client.on('ready', () => {//new ready event
                         role.edit({color : "RANDOM"});
                     };
         });
-    }, 5000);//the rainbow time
+    }, 500);//the rainbow time
   })
   
     
